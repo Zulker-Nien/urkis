972bcf66ef5d7b6d371ad5d066aaee2a4ebdc9d2
Zulker Nien <zulkerb9b@gmail.com>
2024-10-17 11:43:37 +0600

    testing hook for deployment

--- src/app/projects/Techstack.tsx
+++ src/app/projects/Techstack.tsx
@@ -3,6 +3,7 @@ import { useSpring, animated, useInView } from "@react-spring/web";
 import { projectStore } from "@/store/store";
 import { projectType } from "@/utils/types";
 
+// Define tech stack items
 const techStackItems = [
   "All Projects",
   "Typescript",
@@ -13,10 +14,22 @@ const techStackItems = [
   "GraphQL",
 ];
 
+const useTechSpring = (index: number, inView: boolean) => {
+  return useSpring({
+    opacity: inView ? 1 : 0,
+    transform: inView ? "translateX(0)" : "translateX(100px)",
+    config: { tension: 120, friction: 14 },
+    delay: index * 100,
+  });
+};
+
 const Techstack = () => {
+  // InView detection
   const [ref, inView] = useInView({
     rootMargin: "-50px 0px",
   });
+
+  // Project store state
   const selectedStack = projectStore(
     (state: projectType) => state.selectedStack
   );
@@ -24,30 +37,25 @@ const Techstack = () => {
     (state: projectType) => state.setSelectedStack
   );
 
-  const techStylesArray = techStackItems.map((_, index) =>
-    useSpring({
-      opacity: inView ? 1 : 0,
-      transform: inView ? "translateX(0)" : "translateX(100px)",
-      config: { tension: 120, friction: 14 },
-      delay: index * 100,
-    })
-  );
-
   return (
     <div className="w-full flex h-full m-0 p-0 ">
       <div className="w-full flex items-center justify-center gap-8" ref={ref}>
-        {techStackItems.map((item, index) => (
-          <animated.h4
-            key={item}
-            style={techStylesArray[index]} 
-            className={`min-w-max cursor-pointer ${
-              selectedStack === item ? "text-gray-800" : "text-white"
-            }`}
-            onClick={() => setSelectedStack(item)}
-          >
-            {item}
-          </animated.h4>
-        ))}
+        {techStackItems.map((item, index) => {
+          const techStyles = useTechSpring(index, inView);
+
+          return (
+            <animated.h4
+              key={item}
+              style={techStyles}
+              className={`min-w-max cursor-pointer ${
+                selectedStack === item ? "text-gray-800" : "text-white"
+              }`}
+              onClick={() => setSelectedStack(item)}
+            >
+              {item}
+            </animated.h4>
+          );
+        })}
       </div>
     </div>
   );
