acab356ead2acffbe0b5aaddcba673c5b7205440
Zulker Nien <zulkerb9b@gmail.com>
2026-09-16 02:09:11 +0600

    Feat: Added Url

--- src/lib/site.ts
+++ src/lib/site.ts
@@ -1,5 +1,6 @@
 export const SITE_URL =
   process.env.NEXT_PUBLIC_SITE_URL ??
+  process.env.NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL ??
   (process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : "http://localhost:3000");
 
 export const SITE_NAME = "Urkis";
